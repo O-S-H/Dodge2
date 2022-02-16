088e4f8aae31e2b5ffc291ee4bd161915fb4dd8d
O-S-H <dhtjdgus153@naver.com>
2022-02-16 19:54:32 +0900

    2-16복습
    닷지 복습

--- Dodge2/Assets/PlayerCotroller.cs
+++ Dodge2/Assets/PlayerCotroller.cs
@@ -11,34 +11,43 @@ public class PlayerCotroller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, speed);
+        //if (Input.GetKey(KeyCode.UpArrow) == true)
+        //{
+        //    playerRigidbody.AddForce(0f, 0f, speed);
 
-        }
-        if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, -speed);
+        //}
+        //if (Input.GetKey(KeyCode.DownArrow) == true)
+        //{
+        //    playerRigidbody.AddForce(0f, 0f, -speed);
 
-        }
-        if (Input.GetKey(KeyCode.RightArrow) == true)
-        {
-            playerRigidbody.AddForce(speed, 0f, 0f   );
+        //}
+        //if (Input.GetKey(KeyCode.RightArrow) == true)
+        //{
+        //    playerRigidbody.AddForce(speed, 0f, 0f   );
 
-        }
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-            playerRigidbody.AddForce(-speed, 0f, 0f);
+        //}
+        //if (Input.GetKey(KeyCode.LeftArrow) == true)
+        //{
+        //    playerRigidbody.AddForce(-speed, 0f, 0f);
+        float xInput = Input.GetAxis("Horizaontal");
+        float zInput = Input.GetAxis("Vertical");
 
-        }
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        Vector3 newveloctiy = new Vector3(xSpeed, 0f,zSpeed);
+        playerRigidbody.velocity = newveloctiy;
+
+        
        
     }
+
     public void Die()
     {
         gameObject.SetActive(false);
