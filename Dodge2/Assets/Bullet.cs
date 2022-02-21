d5cf7360633015e74bb36f1c62a38d4a6567ca3a
O-S-H <dhtjdgus153@naver.com>
2022-02-21 22:10:21 +0900

    2/21
    Bullet 진행중

---
+++ Dodge2/Assets/Bullet.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    private Rigidbody bulletRigdbody;
+
+    void Start()
+    {
+        
+    }
+
+   
+}
